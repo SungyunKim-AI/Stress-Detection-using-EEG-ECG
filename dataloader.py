9791a1375e6d0215ce0afa5f0a3a98a8e6aed876
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-02-04 18:04:14 +0900

    commit

--- dataloader.py
+++ dataloader.py
@@ -2,52 +2,58 @@ import numpy as np
 import scipy.io as sio
 import tensorflow as tf
 
-path=u'DREAMER.mat'
-data=sio.loadmat(path)
+def dataloader():
+    path=u'DREAMER.mat'
+    data=sio.loadmat(path)
     
-# 23명의 실험참여자가 18개의 비디오 클립을 시청하고, 128Hz로 60초간 14개의 채널로 측정한 RAW 데이터
-numOfSubjects = 23      # 실험 참여자 수
-numOfVideo = 18         # 시청한 비디오 클립 수
-numOfEEGChannel = 14    # EEG 측정 채널 수
-numOfECGChannel = 2     # ECG 측정 채널 수
-numOfCategory = 3       # 감성 분류 카테고리 수 (Valence, Arousal, Dominance)
+    # 23명의 실험참여자가 18개의 비디오 클립을 시청하고, 128Hz로 60초간 14개의 채널로 측정한 RAW 데이터
+    numOfSubjects = 23      # 실험 참여자 수
+    numOfVideo = 18         # 시청한 비디오 클립 수
+    numOfEEGChannel = 14    # EEG 측정 채널 수
+    numOfECGChannel = 2     # ECG 측정 채널 수
+    numOfCategory = 3       # 감성 분류 카테고리 수 (Valence, Arousal, Dominance)
 
-EEG_baseline = []
-EEG_stimuli = []
-ECG_baseline = []
-ECG_stimuli = []
-Labels = np.zeros((numOfCategory, numOfSubjects, numOfVideo))
+    EEG_baseline = []
+    EEG_stimuli = []
+    ECG_baseline = []
+    ECG_stimuli = []
+    Labels = np.zeros((numOfCategory, numOfSubjects * numOfVideo))
 
-for iter_subject in range(0, numOfSubjects):
-    for iter_video in range(0, numOfVideo):
-        for iter_ch in range(0, numOfEEGChannel):
-            baseline_e = data['DREAMER'][0,0]['Data'][0,iter_subject]['EEG'][0,0]['baseline'][0,0][iter_video,0][:,iter_ch]
-            stimuli_e  = data['DREAMER'][0,0]['Data'][0,iter_subject]['EEG'][0,0]['stimuli'][0,0][iter_video,0][-7808:,iter_ch]
-        EEG_baseline.append(baseline_e)
-        EEG_stimuli.append(stimuli_e)
+    for iter_subject in range(0, numOfSubjects):
+        for iter_video in range(0, numOfVideo):
+            baseline_e = data['DREAMER'][0,0]['Data'][0,iter_subject]['EEG'][0,0]['baseline'][0,0][iter_video,0][:,:]
+            stimuli_e  = data['DREAMER'][0,0]['Data'][0,iter_subject]['EEG'][0,0]['stimuli'][0,0][iter_video,0][-7808:,:]
+            EEG_baseline.append(baseline_e)
+            EEG_stimuli.append(stimuli_e)
 
-        for iter_ch in range(0, numOfECGChannel):
-            baseline_c = data['DREAMER'][0,0]['Data'][0,iter_subject]['ECG'][0,0]['baseline'][0,0][iter_video,0][:,iter_ch]
-            stimuli_c = data['DREAMER'][0,0]['Data'][0,iter_subject]['ECG'][0,0]['stimuli'][0,0][iter_video,0][-15616:,iter_ch] 
-        ECG_baseline.append(baseline_c)
-        ECG_stimuli.append(stimuli_c)
+            baseline_c = data['DREAMER'][0,0]['Data'][0,iter_subject]['ECG'][0,0]['baseline'][0,0][iter_video,0][:,:]
+            stimuli_c = data['DREAMER'][0,0]['Data'][0,iter_subject]['ECG'][0,0]['stimuli'][0,0][iter_video,0][-15616:,:] 
+            ECG_baseline.append(baseline_c)
+            ECG_stimuli.append(stimuli_c)
 
-        Labels[0, iter_subject, iter_video] = data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreValence'][0,0][iter_video, 0]
-        Labels[1, iter_subject, iter_video] = data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreArousal'][0,0][iter_video,0]
-        Labels[2, iter_subject, iter_video] = data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreDominance'][0,0][iter_video,0]
+        Labels[0].append(data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreValence'][0,0][:, 0])
+        Labels[1].append(data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreArousal'][0,0][:,0])
+        Labels[2].append(data['DREAMER'][0,0]['Data'][0,iter_subject]['ScoreDominance'][0,0][:,0])
 
-EEG_baseline = np.asarray(EEG_baseline)
-EEG_stimuli = np.asarray(EEG_stimuli)
-ECG_baseline = np.asarray(ECG_baseline)
-ECG_stimuli = np.asarray(ECG_stimuli)
+    EEG_baseline = np.asarray(EEG_baseline)
+    EEG_baseline = np.swapaxes(EEG_baseline, 1, 2)
+    EEG_stimuli = np.asarray(EEG_stimuli)
+    EEG_stimuli = np.swapaxes(EEG_stimuli, 1, 2)
+    ECG_baseline = np.asarray(ECG_baseline)
+    ECG_baseline = np.swapaxes(ECG_baseline, 1, 2)
+    ECG_stimuli = np.asarray(ECG_stimuli)
+    ECG_stimuli = np.swapaxes(ECG_stimuli, 1, 2)
 
-# Tensorflow 배열 타입으로 변형
-EEG_baseline = tf.convert_to_tensor(EEG_baseline, dtype=tf.float32)
-EEG_stimuli = tf.convert_to_tensor(EEG_stimuli, dtype=tf.float32)
-ECG_baseline = tf.convert_to_tensor(ECG_baseline, dtype=tf.float32)
-ECG_stimuli = tf.convert_to_tensor(ECG_stimuli, dtype=tf.float32)
+    # Tensorflow 배열 타입으로 변형
+    EEG_baseline = tf.convert_to_tensor(EEG_baseline, dtype=tf.float32)
+    EEG_stimuli = tf.convert_to_tensor(EEG_stimuli, dtype=tf.float32)
+    ECG_baseline = tf.convert_to_tensor(ECG_baseline, dtype=tf.float32)
+    ECG_stimuli = tf.convert_to_tensor(ECG_stimuli, dtype=tf.float32)
 
-print(EEG_baseline.shape)
-print(EEG_stimuli.shape)
-print(ECG_baseline.shape)
-print(ECG_stimuli.shape)
+    print(EEG_baseline.shape)           # 414, 14, 7808
+    print(EEG_stimuli.shape)
+    print(ECG_baseline.shape)
+    print(ECG_stimuli.shape)
+    print(Labels.shape)
+
+    return EEG_baseline, EEG_stimuli, ECG_baseline, ECG_stimuli, Labels
--- sample_ERP.py
+++ sample_ERP.py
@@ -1,58 +1,3 @@
-"""
- Sample script using EEGNet to classify Event-Related Potential (ERP) EEG data
- from a four-class classification task, using the sample dataset provided in
- the MNE [1, 2] package:
-     https://martinos.org/mne/stable/manual/sample_dataset.html#ch-sample-data
-   
- The four classes used from this dataset are:
-     LA: Left-ear auditory stimulation
-     RA: Right-ear auditory stimulation
-     LV: Left visual field stimulation
-     RV: Right visual field stimulation
- The code to process, filter and epoch the data are originally from Alexandre
- Barachant's PyRiemann [3] package, released under the BSD 3-clause. A copy of 
- the BSD 3-clause license has been provided together with this software to 
- comply with software licensing requirements. 
- 
- When you first run this script, MNE will download the dataset and prompt you
- to confirm the download location (defaults to ~/mne_data). Follow the prompts
- to continue. The dataset size is approx. 1.5GB download. 
- 
- For comparative purposes you can also compare EEGNet performance to using 
- Riemannian geometric approaches with xDAWN spatial filtering [4-8] using 
- PyRiemann (code provided below).
- [1] A. Gramfort, M. Luessi, E. Larson, D. Engemann, D. Strohmeier, C. Brodbeck,
-     L. Parkkonen, M. Hämäläinen, MNE software for processing MEG and EEG data, 
-     NeuroImage, Volume 86, 1 February 2014, Pages 446-460, ISSN 1053-8119.
- [2] A. Gramfort, M. Luessi, E. Larson, D. Engemann, D. Strohmeier, C. Brodbeck, 
-     R. Goj, M. Jas, T. Brooks, L. Parkkonen, M. Hämäläinen, MEG and EEG data 
-     analysis with MNE-Python, Frontiers in Neuroscience, Volume 7, 2013.
- [3] https://github.com/alexandrebarachant/pyRiemann. 
- [4] A. Barachant, M. Congedo ,"A Plug&Play P300 BCI Using Information Geometry"
-     arXiv:1409.0107. link
- [5] M. Congedo, A. Barachant, A. Andreev ,"A New generation of Brain-Computer 
-     Interface Based on Riemannian Geometry", arXiv: 1310.8115.
- [6] A. Barachant and S. Bonnet, "Channel selection procedure using riemannian 
-     distance for BCI applications," in 2011 5th International IEEE/EMBS 
-     Conference on Neural Engineering (NER), 2011, 348-351.
- [7] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, “Multiclass 
-     Brain-Computer Interface Classification by Riemannian Geometry,” in IEEE 
-     Transactions on Biomedical Engineering, vol. 59, no. 4, p. 920-928, 2012.
- [8] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, “Classification of 
-     covariance matrices using a Riemannian-based kernel for BCI applications“, 
-     in NeuroComputing, vol. 112, p. 172-178, 2013.
- Portions of this project are works of the United States Government and are not
- subject to domestic copyright protection under 17 USC Sec. 105.  Those 
- portions are released world-wide under the terms of the Creative Commons Zero 
- 1.0 (CC0) license.  
- 
- Other portions of this project are subject to domestic copyright protection 
- under 17 USC Sec. 105.  Those portions are licensed under the Apache 2.0 
- license.  The complete text of the license governing this material is in 
- the file labeled LICENSE.TXT that is a part of this project's official 
- distribution. 
-"""
-
 import numpy as np
 
 # mne imports
@@ -76,38 +21,44 @@ from sklearn.linear_model import LogisticRegression
 # tools for plotting confusion matrices
 from matplotlib import pyplot as plt
 
+from dataloader import dataloader
+
 # while the default tensorflow ordering is 'channels_last' we set it here
 # to be explicit in case if the user has changed the default ordering
-K.set_image_data_format('channels_last')
+# K.set_image_data_format('channels_last')
 
-##################### Process, filter and epoch the data ######################
-data_path = sample.data_path()
+# ##################### Process, filter and epoch the data ######################
+# data_path = sample.data_path()
 
-# Set parameters and read data
-raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
-event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
-tmin, tmax = -0., 1
-event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
+# # Set parameters and read data
+# raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+# event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
+# tmin, tmax = -0., 1
+# event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
 
-# Setup for reading the raw data
-raw = io.Raw(raw_fname, preload=True, verbose=False)
-raw.filter(2, None, method='iir')  # replace baselining with high-pass
-events = mne.read_events(event_fname)
+# # Setup for reading the raw data
+# raw = io.Raw(raw_fname, preload=True, verbose=False)
+# raw.filter(2, None, method='iir')  # replace baselining with high-pass
+# events = mne.read_events(event_fname)
 
-raw.info['bads'] = ['MEG 2443']  # set bad channels
-picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
-                       exclude='bads')
+# raw.info['bads'] = ['MEG 2443']  # set bad channels
+# picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
+#                        exclude='bads')
 
-# Read epochs
-epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=False,
-                    picks=picks, baseline=None, preload=True, verbose=False)
-labels = epochs.events[:, -1]
+# # Read epochs
+# epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=False,
+#                     picks=picks, baseline=None, preload=True, verbose=False)
+# labels = epochs.events[:, -1]
 
 # extract raw data. scale by 1000 due to scaling sensitivity in deep learning
-X = epochs.get_data()*1000 # format is in (trials, channels, samples)
-y = labels
+# X = epochs.get_data()*1000 # format is in (trials, channels, samples)
+# y = labels
 
-kernels, chans, samples = 1, 60, 151
+EEG_baseline, EEG_stimuli, ECG_baseline, ECG_stimuli, Labels = dataloader()
+X = EEG_stimuli
+y = Labels
+
+kernels, chans, samples = 1, 14, 7808
 
 # take 50/25/25 percent of the data to train/validate/test
 X_train      = X[0:144,]
