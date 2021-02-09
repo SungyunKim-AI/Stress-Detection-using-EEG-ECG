d34978d54882da0f627eb1a2c955b5f6a381ae25
KimSungYoon-KOK <sungyun1997@gmail.com>
2021-02-10 02:24:49 +0900

    Update preprocessing.m

--- preprocessing.m
+++ preprocessing.m
@@ -6,30 +6,38 @@
 % 5. Cohen [32]에서 권장하는 CAR (Common Average Reference)
 
 filepath = 'C:\Users\user\Desktop\Graduation-Project\DREAMER.mat';
+% filepath = '/Users/kok_ksy/Documents/GitHub/Graduation-Project/DREAMER.mat';
 Dataset = load(filepath);
 subjects = Dataset.DREAMER.Data;
+
 eeglab;
 
-for i = 1:23
-    tmpSub = subjects(1,i);
-    subject = tmpSub{1};
-    for j = 1:18
-        baseline = FIRfiltering(subject.EEG.baseline(j,1));
-        stimuli = FIRfiltering(subject.EEG.stimuli(j,1));
-        %pop_clean_rawdata()
-    end
-end
+tmpSub = subject(1,1);
+subject = tmpSub{1};
+baseline = subject.EEG.baseline(1,1);
+stimuli = subject.EEG.stimuli(1,1);
 
-function filtedData = FIRfiltering(input)
-    val = input{1};
-    overall = fir1(9, [0.0625, 0.46875]);
-    theta = fir1(9, [0.0625, 0.125]);
-    alpha = fir1(9, [0.125, 0.203125]);
-    beta = fir1(9, [0.203125, 0.46875]);
-    
-    filted = filter(overall, 1, val);
-    filtedTheta = filter(theta, 1, filted);
-    filtedAlpha = filter(alpha, 1, filted);
-    filtedBeta = filter(beta, 1, filted);
-    filtedData = [filtedTheta, filtedAlpha, filtedBeta];
-end
+
+% for i = 1:23
+%     tmpSub = subjects(1,i);
+%     subject = tmpSub{1};
+%     for j = 1:18
+%         baseline = FIRfiltering(subject.EEG.baseline(j,1));
+%         stimuli = FIRfiltering(subject.EEG.stimuli(j,1));
+%         %pop_clean_rawdata()
+%     end
+% end
+% 
+% function filtedData = FIRfiltering(input)
+%     val = input{1};
+%     overall = fir1(9, [0.0625, 0.46875]);
+%     theta = fir1(9, [0.0625, 0.125]);
+%     alpha = fir1(9, [0.125, 0.203125]);
+%     beta = fir1(9, [0.203125, 0.46875]);
+%     
+%     filted = filter(overall, 1, val);
+%     filtedTheta = filter(theta, 1, filted);
+%     filtedAlpha = filter(alpha, 1, filted);
+%     filtedBeta = filter(beta, 1, filted);
+%     filtedData = [filtedTheta, filtedAlpha, filtedBeta];
+% end
