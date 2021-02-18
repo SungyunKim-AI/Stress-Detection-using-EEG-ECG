dc844064d798d07875a6cfe8f0fe96a9afb04b65
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-02-18 11:38:49 +0900

    Update preprocessing.m

--- preprocessing.m
+++ preprocessing.m
@@ -9,22 +9,47 @@ filepath = 'C:\Users\user\Desktop\Graduation-Project\DREAMER.mat';
 % filepath = '/Users/kok_ksy/Documents/GitHub/Graduation-Project/DREAMER.mat';
 Dataset = load(filepath);
 subjects = Dataset.DREAMER.Data;
-
 eeglab;
 
-tmpSub = subject(1,1);
+tmpSub = subjects(1,1);
 subject = tmpSub{1};
 baseline = subject.EEG.baseline(1,1);
+baseline = baseline{1}.';
 stimuli = subject.EEG.stimuli(1,1);
+stimuli = stimuli{1}.';
+
+NumOfSub = 23;
+NumOfClip = 18;
+
+for sub = 1:NumOfSub
+    for clip = 1:NumOfClip
+    end
+end
+
+% 0. Data import
+%EEG = pop_importdata('dataformat','array','nbchan',0,'data','baseline','srate',128,'pnts',0,'xmin',0);
+
+% % 1. Hamming sinc linear phase FIR filters : 4 ~ 30Hz
+% EEG = pop_firws(EEG, 'fcutoff', [30 4], 'ftype', 'bandpass', 'wtype', 'hamming', 'forder', 48, 'minphase', 0, 'usefftfilt', 0, 'plotfresp', 0, 'causal', 0);
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'setname','filted_baseline','gui','off'); 
+% EEG = pop_firws(EEG, 'fcutoff', [8 4], 'ftype', 'bandpass', 'wtype', 'hamming', 'forder', 48, 'minphase', 0, 'usefftfilt', 0, 'plotfresp', 0, 'causal', 0);
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'setname','filtedtheta_baseline','gui','off'); 
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'retrieve',2,'study',0); 
+% EEG = pop_firws(EEG, 'fcutoff', [13 8], 'ftype', 'bandpass', 'wtype', 'hamming', 'forder', 48, 'minphase', 0, 'usefftfilt', 0, 'plotfresp', 0, 'causal', 0);
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'setname','filtedalpha_baseline','gui','off'); 
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 4,'retrieve',2,'study',0); 
+% EEG = pop_firws(EEG, 'fcutoff', [30 12], 'ftype', 'bandpass', 'wtype', 'hamming', 'forder', 48, 'minphase', 0, 'usefftfilt', 0, 'plotfresp', 0, 'causal', 0);
+% [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'setname','filtedbeta_baseline','gui','off'); 
+
+% plot(ALLEEG(2).data)
 
 
-% for i = 1:23
+% for i = 1:2
 %     tmpSub = subjects(1,i);
 %     subject = tmpSub{1};
 %     for j = 1:18
 %         baseline = FIRfiltering(subject.EEG.baseline(j,1));
 %         stimuli = FIRfiltering(subject.EEG.stimuli(j,1));
-%         %pop_clean_rawdata()
 %     end
 % end
 % 
@@ -37,7 +62,10 @@ stimuli = subject.EEG.stimuli(1,1);
 %     
 %     filted = filter(overall, 1, val);
 %     filtedTheta = filter(theta, 1, filted);
+%     plot(filtedTheta);
 %     filtedAlpha = filter(alpha, 1, filted);
+%     plot(filtedAlpha);
 %     filtedBeta = filter(beta, 1, filted);
+%     plot(filtedBeta);
 %     filtedData = [filtedTheta, filtedAlpha, filtedBeta];
 % end
