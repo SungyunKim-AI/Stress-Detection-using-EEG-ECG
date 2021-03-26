212590a92fa82d8a924d73000d7cf60822da57cc
Inae Kim <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-03-26 23:41:18 +0900

    commit

--- ECG_test/ecg_model.py
+++ ECG_test/ecg_model.py
@@ -11,19 +11,15 @@ def generate_model():
 
     # 모델 구성하기
     make_model = Sequential([
-        Input(shape=(2900, 1), name='input_layer'),
+        Input(shape=(10240, 1), name='input_layer'),
         Conv1D(
-            filters=15,
+            filters=30,
             kernel_size=10,
             strides=1,
             padding="same",
             activation='relu',
             name='conv_layer1'
         ),
-        MaxPooling1D(
-            pool_size=2
-        ),
-        Dropout(0.1),
         Conv1D(
             filters=30,
             kernel_size=10,
@@ -31,9 +27,6 @@ def generate_model():
             padding="same",
             activation='relu',
             name='conv_layer2'
-        ),
-        MaxPooling1D(
-            pool_size=2
         ),
         Dropout(0.1),
         Conv1D(
@@ -56,9 +49,8 @@ def generate_model():
             name='conv_layer4'
         ),
         MaxPooling1D(
-            pool_size=2
+            pool_size=4
         ),
-        Dropout(0.1),
         Flatten(),
         Dense(128, activation='relu'),
         Dense(2, activation='softmax', name='output_layer')
--- ECG_test/train_ecg.py
+++ ECG_test/train_ecg.py
@@ -15,10 +15,10 @@ from ecg_model import generate_model
 
 
 ECG, Labels = load_ecg_data()
-ECG = normalization(ECG, "01")
+#ECG = normalization(ECG, "01")
 
-ECG = np.array(ECG)
-Labels = np.array(Labels)
+#ECG = np.array(ECG)
+#Labels = np.array(Labels)
 
 X_train, X_validate, Y_train, Y_validate = train_test_split(
     ECG, Labels, test_size=0.3, random_state=42)
@@ -26,7 +26,7 @@ X_validate, X_test, Y_validate, Y_test = train_test_split(
     X_validate, Y_validate, test_size=0.5, random_state=42)
 
 
-kernels, chans, samples = 1, 1, 2900
+kernels, chans, samples = 1, 1, 10240
 
 X_train = X_train.reshape(X_train.shape[0], samples, chans)
 X_validate = X_validate.reshape(X_validate.shape[0], samples, chans)
@@ -45,8 +45,8 @@ model.compile(
 fit_model = model.fit(
     X_train,
     Y_train,
-    epochs=100,
-    batch_size=10,
+    epochs=300,
+    batch_size=20,
     validation_data=(X_validate, Y_validate)
 )
 
