e27bff810979df34374c409c8ef5cda7c6b20420
KimSungYoon-KOK <sungyun1997@gmail.com>
2021-03-26 19:41:40 +0900

    Update load_ecg_data.py

--- ECG_test/load_ecg_data.py
+++ ECG_test/load_ecg_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 import glob
 from matplotlib import pyplot as plt
 
@@ -12,6 +11,7 @@ def plot(data):
     plt.xlabel('time')
     plt.plot(data)
     plt.show()
+
 
 
 def load_ecg_data():
@@ -47,25 +47,6 @@ def load_ecg_data():
     print(Labels.shape)
 
     return ECG, Labels
-    # for videoIdx in tqdm(range(0, numOfVideo), desc='VideoIdx'):
-    #     fileName_labels = path_labelset + str(videoIdx+1) + ".csv"
-    #     labels = pd.read_csv(fileName_labels, header=None)      # 32 x 4
-    #     labels = np.array(labels)
-
-    #     for subIdx in range(0, numOfSubjects):
-    #         fileName_data = path_dataset + \
-    #             str(videoIdx+1) + "_" + str(subIdx+1) + ".csv"
-    #         eeg = pd.read_csv(fileName_data, header=None)       # 14 x 8064
-    #         EEG.append(np.array(eeg))
-    #         Labels.append(labels[subIdx])
-
-    # Labels = np.array(Labels).T
-    # EEG = np.array(EEG)
-    # print("Labels.shape = ", Labels.shape)    # (1280, 4)
-    # print("EEG.shape = ", EEG.shape)          # (1280, 14, 8064)
-
-    # return EEG, Labels
 
 
-# test_dataloader()
 load_ecg_data()
