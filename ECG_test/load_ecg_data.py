18b46522ae1d33d0e0faf13e73e0ad9f8311ede9
KimSungYoon-KOK <sungyun1997@gmail.com>
2021-03-26 21:36:59 +0900

    Update load_ecg_data.py

--- ECG_test/load_ecg_data.py
+++ ECG_test/load_ecg_data.py
@@ -5,22 +5,26 @@ import glob
 def load_ecg_data():
     print("ECG Data Loading...")
 
-    Labels = []
-    ECG = []
+    Fs = 512    # Sample Frequency (Hz)
+    Ss = 20     # Sample second (sec)
+    step = 3    # Overlapping Step (sec)
+    cptTime = 62
 
-    # 데이터 경로 설정
-    baseline_paths = glob.glob(
-        "C:/Users/user/Desktop/data_preprocessed/ECG_preprocessed/normalized_data/baseline/*")
-    stimuli_paths = glob.glob(
-        "C:/Users/user/Desktop/data_preprocessed/ECG_preprocessed/normalized_data/stimuli/*")
+    # Dataset Path
+    baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ECG_preprocessed/normalized_data/baseline/*")
+    stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ECG_preprocessed/normalized_data/stimuli/*")
+
+    ECG = []
+    Labels = []
 
     for category in [baseline_paths, stimuli_paths]:
         for path in category:
             data = pd.read_csv(path, header=None)
             
             # data overlapping
-            for i in range(0,12):
-                part_data = data[i*512:10240+i*512][0]
+            
+            for i in range(0, cptTime - Ss, step):
+                part_data = data[i*Fs : (i+Ss)*Fs][2]
                 ECG.append(part_data)
 
                 # Labels one-hot encoding
