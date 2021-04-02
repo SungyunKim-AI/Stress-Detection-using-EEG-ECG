dafd561e06aab57d636b161ee51899657dd7f2ce
KimSungYoon-KOK <sungyun1997@gmail.com>
2021-04-02 21:56:25 +0900

    Update load_eeg_data.py

--- EEG_train/load_eeg_data.py
+++ EEG_train/load_eeg_data.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 import glob
+import os
 from tqdm import tqdm
 
+# 전체 세트 오버래핑
 def load_eeg_data():
     print("EEG Data Loading...")
 
@@ -12,12 +14,12 @@ def load_eeg_data():
     samples = Fs * Ss
 
     # Dataset Path _ ASR + CAR
-    # baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/baseline/alpha/*")
-    # stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/stimuli/alpha/*")
+    baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/baseline/alpha/*")
+    stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/stimuli/alpha/*")
 
     # Dataset Path _ CAR
-    baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/baseline/alpha/*")
-    stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/stimuli/alpha/*")
+    # baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/baseline/alpha/*")
+    # stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/stimuli/alpha/*")
 
     EEG = []
     Labels = []
@@ -53,6 +55,83 @@ def load_eeg_data():
     print("Samples : ", samples)
 
     return EEG, Labels, numOfBaseline, numOfStimuli, samples
+
+
+# Train, Test, Validate 미리 나눠서 오버레핑하고 셔플
+def load_eeg_data_2():
+    print("EEG Data Loading...")
+
+    Subjects = 10
+    Fs = 128    # Sample Frequency (Hz)
+    Ss = 40     # Sample second (sec)
+    step = 2    # Overlapping Step (sec)
+    samples = Fs * Ss
+
+    # Dataset Path _ ASR + CAR
+    baseline_paths = "C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/baseline/alpha/"
+    stimuli_paths = "C:/Users/user/Desktop/data_preprocessed/ASR_CAR_preprocessed/EEG/stimuli/alpha/"
+
+    # Dataset Path _ CAR
+    # baseline_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/baseline/alpha/*")
+    # stimuli_paths = glob.glob("C:/Users/user/Desktop/data_preprocessed/CAR_preprocessed/EEG/stimuli/alpha/*")
+
+    x_Train = []
+    x_Test = []
+    x_Validate = []
+    y_Train = []
+    y_Test =[]
+    y_Validate = []
+
+    for category in range(2):
+        for subject in tqdm(range(Subjects)):
+            for sample in range(10):
+                path = baseline_paths + "s" + str(subject) + "_" + str(sample)
+                dataset = pd.read_csv(path, header=None)
+                data_frames = pd.DataFrame(dataset)
+                data = np.array(data_frames.values)
+
+                # data overlapping
+                for i in range(0, int(data.shape[1]/Fs) - Ss + 1, step):
+                    part_data = data[0:13, (i*Fs) : ((i+Ss)*Fs)]
+                    
+                    if subject == 10:
+                        x_Validate.append(part_data)
+                        y_Validate.append(label_append(category, subject))
+                    elif subject == 9:
+                        x_Test.append(part_data)
+                        y_Test.append(label_append(category, subject))
+                    else:
+                        x_Train.append(part_data)
+                        y_Test.append(label_append(category, subject))
+
+    [x_Train, y_Train] = data_shuffle(x_Train, y_Train)
+    [x_Test, y_Test] = data_shuffle(x_Test, y_Test)
+    [x_Validate, y_Validate] = data_shuffle(x_Validate, y_Validate)
+
+    print("Train Data Shape : ", x_Train.shape)       # (3032, )
+    print("Test Data Shape : ", x_Test.shape)
+    print("Validate Data Shape : ", x_Validate.shape)
+
+
+    return x_Train, x_Test, x_Validate, y_Train, y_Test, y_Validate
+
+def label_append(category, subject):
+    labels = []
+    # Labels one-hot encoding
+    if category == 0:       # 0: baseline
+        labels.append([1, 0])
+    elif category == 1:     # 1: stimuli
+        labels.append([0, 1])
+    return labels
+
+def data_shuffle(x, y):
+    s = np.arange(x.shape[0])
+    np.random.shuffle(s)
+
+    x = x[s]
+    y = y[s]
+    
+    return x, y
 
 #[EEG, Labels, numOfBaseline, numOfStimuli, samples] = load_eeg_data()
 
